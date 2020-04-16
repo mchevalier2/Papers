8ba4a78a7f29803d553da11557e7fa430ddb386d
Manu Chevalier <chevalier.manuel@gmail.com>
2020-04-16 18:19:35 +0200

    Update makeFigDR3.R

--- Chevalier_etal_MD962048/figures/makeFigDR3.R
+++ Chevalier_etal_MD962048/figures/makeFigDR3.R
@@ -70,7 +70,7 @@ if (makePlot) {
 
     pdf(paste0(OUTPUT_FOLDER, "/Chevalier_etal_MD962048_FigDR3.pdf"), width=7.48, height=7, useDingbats=FALSE)  ;  {
         par(ps=7,bg=makeTransparent("white",alpha=0),mar=rep(0,4),cex=1,cex.main=1)
-        layout(matrix(1:6, ncol=3, byrow=TRUE), width=1, height=1)
+        layout(matrix(1:6, ncol=2, byrow=TRUE), width=1, height=1)
 
         plot.new()  ;  { ## SSTs
             plot.window(xlim=c(-100,900),ylim=range(MAT[,2])+diff(range(MAT[,2]))*c(-0.1,0.02),main='',ylab='',xlab='')  ;  {
