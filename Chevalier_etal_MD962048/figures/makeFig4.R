f861bf93f2bccc911e28ccfb9727f2046994f4e0
Manu Chevalier <chevalier.manuel@gmail.com>
2020-08-27 15:36:16 +0200

    Update makeFig4.R
    Adapting to Geology formatting.

--- Chevalier_etal_MD962048/figures/makeFig4.R
+++ Chevalier_etal_MD962048/figures/makeFig4.R
@@ -82,122 +82,122 @@ if (makePlot) {
 
     MIS.col=makeTransparent(c(rgb(205,91,65,maxColorValue=255),rgb(255,127,80,maxColorValue=255),rgb(122,197,205,maxColorValue=255),rgb(152,245,255,maxColorValue=255), 'white'), alpha=0.2)
 
-    pdf(paste0(OUTPUT_FOLDER, "/Chevalier_etal_MD962048_Fig4.pdf"), width=3.54, height=9.45, useDingbats=FALSE)  ;  {
+    pdf(paste0(OUTPUT_FOLDER, "/Chevalier_etal_MD962048_Fig4.pdf"), width=2.35, height=7.25, useDingbats=FALSE)  ;  {
       par(ps=7,bg=makeTransparent("white",alpha=0),mar=rep(0,4),cex=1,cex.main=1)
       plot.new()
 
-      plot.window(xlim=c(-150,950),ylim=c(-0.0125,1.0125),main='',ylab='',xlab='') ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(-0.0125,1.0125),main='',ylab='',xlab='') ;  {
           ## Top part
           for(i in c(11.7, 29,57,71,130,191,243,300,337,374,424,478,533,565,621,676,712,761,790)){  segments(i,0,i,1.01, col="grey50", lwd=0.5, lty=2)  }
-          rect(-9,0,11.7,1, col=MIS.col[5], border=NA)  ;  rect(-9,1.02,11.7,1, col="white", border='black', lwd=0.5)  ;  text(1.375,1.01,'1',cex=0.8, adj=c(0.5,0.5))
-          rect(11.7,0,29,1, col=MIS.col[4], border=NA)  ;  rect(29,1.02,11.7,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(20.35,1.01,'2',cex=0.8, adj=c(0.5,0.5))
-          rect(29,0,57,1, col=MIS.col[3], border=NA)  ;  rect(29,1.02,57,1, col=MIS.col[3], border='black', lwd=0.5)  ;  text(43,1.01,'3',cex=0.8, adj=c(0.5,0.5))
-          rect(57,0,71,1, col=MIS.col[4], border=NA)  ;  rect(57,1.02,71,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(64,1.01,'4',cex=0.8, adj=c(0.5,0.5))
-          rect(71,0,130,1, col=MIS.col[5], border=NA)  ;  rect(71,1.02,130,1, col="white", border='black', lwd=0.5)  ;  text(100.5,1.01,'5',cex=0.8, adj=c(0.5,0.5))
-          rect(130,0,191,1, col=MIS.col[4], border=NA)  ;  rect(130,1.02,191,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(160.5,1.01,'6',cex=0.8, adj=c(0.5,0.5))
-          rect(191,0,243,1, col=MIS.col[5], border=NA)  ;  rect(191,1.02,243,1, col="white", border='black', lwd=0.5)  ;  text(217,1.01,'7',cex=0.8, adj=c(0.5,0.5))
-          rect(243,0,300,1, col=MIS.col[4], border=NA)  ;  rect(243,1.02,300,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(271.5,1.01,'8',cex=0.8, adj=c(0.5,0.5))
-          rect(300,0,337,1, col=MIS.col[5], border=NA)  ;  rect(300,1.02,337,1, col="white", border='black', lwd=0.5)  ;  text(318.5,1.01,'9',cex=0.8, adj=c(0.5,0.5))
-          rect(337,0,374,1, col=MIS.col[4], border=NA)  ;  rect(337,1.02,374,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(355.5,1.01,'10',cex=0.8, adj=c(0.5,0.5))
-          rect(374,0,424,1, col=MIS.col[5], border=NA)  ;  rect(374,1.02,424,1, col="white", border='black', lwd=0.5)  ;  text(399,1.01,'11',cex=0.8, adj=c(0.5,0.5))
-          rect(424,0,478,1, col=MIS.col[4], border=NA)  ;  rect(424,1.02,478,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(451,1.01,'12',cex=0.8, adj=c(0.5,0.5))
-          rect(478,0,533,1, col=MIS.col[5], border=NA)  ;  rect(478,1.02,533,1, col="white", border='black', lwd=0.5)  ;  text(505.5,1.01,'13',cex=0.8, adj=c(0.5,0.5))
-          rect(533,0,563,1, col=MIS.col[4], border=NA)  ;  rect(533,1.02,563,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(548,1.01,'14',cex=0.8, adj=c(0.5,0.5))
-          rect(563,0,621,1, col=MIS.col[5], border=NA)  ;  rect(563,1.02,621,1, col="white", border='black', lwd=0.5)  ;  text(592,1.01,'15',cex=0.8, adj=c(0.5,0.5))
-          rect(621,0,676,1, col=MIS.col[4], border=NA)  ;  rect(621,1.02,676,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(648.5,1.01,'16',cex=0.8, adj=c(0.5,0.5))
-          rect(676,0,712,1, col=MIS.col[5], border=NA)  ;  rect(676,1.02,712,1, col="white", border='black', lwd=0.5)  ;  text(694,1.01,'17',cex=0.8, adj=c(0.5,0.5))
-          rect(712,0,761,1, col=MIS.col[4], border=NA)  ;  rect(712,1.02,761,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(736.5,1.01,'18',cex=0.8, adj=c(0.5,0.5))
-          rect(761,0,790,1, col=MIS.col[5], border=NA)  ;  rect(761,1.02,790,1, col="white", border='black', lwd=0.5)  ;  text(775.5,1.01,'19',cex=0.8, adj=c(0.5,0.5))
-          rect(790,0,809,1, col=MIS.col[4], border=NA)  ;  rect(790,1.02,809,1, col=MIS.col[4], border='black', lwd=0.5)  #;  text(790,1.015,'20',cex=0.8, adj=c(0.5,0.5))
+          rect(-9,0,11.7,1, col=MIS.col[5], border=NA)  ;  rect(-9,1.02,11.7,1, col="white", border='black', lwd=0.5)  ;  text(1.375,1.01,'1',cex=0.7, adj=c(0.5,0.5))
+          rect(11.7,0,29,1, col=MIS.col[4], border=NA)  ;  rect(29,1.02,11.7,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(20.35,1.01,'2',cex=0.7, adj=c(0.5,0.5))
+          rect(29,0,57,1, col=MIS.col[3], border=NA)  ;  rect(29,1.02,57,1, col=MIS.col[3], border='black', lwd=0.5)  ;  text(43,1.01,'3',cex=0.7, adj=c(0.5,0.5))
+          rect(57,0,71,1, col=MIS.col[4], border=NA)  ;  rect(57,1.02,71,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(64,1.01,'4',cex=0.7, adj=c(0.5,0.5))
+          rect(71,0,130,1, col=MIS.col[5], border=NA)  ;  rect(71,1.02,130,1, col="white", border='black', lwd=0.5)  ;  text(100.5,1.01,'5',cex=0.7, adj=c(0.5,0.5))
+          rect(130,0,191,1, col=MIS.col[4], border=NA)  ;  rect(130,1.02,191,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(160.5,1.01,'6',cex=0.7, adj=c(0.5,0.5))
+          rect(191,0,243,1, col=MIS.col[5], border=NA)  ;  rect(191,1.02,243,1, col="white", border='black', lwd=0.5)  ;  text(217,1.01,'7',cex=0.7, adj=c(0.5,0.5))
+          rect(243,0,300,1, col=MIS.col[4], border=NA)  ;  rect(243,1.02,300,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(271.5,1.01,'8',cex=0.7, adj=c(0.5,0.5))
+          rect(300,0,337,1, col=MIS.col[5], border=NA)  ;  rect(300,1.02,337,1, col="white", border='black', lwd=0.5)  ;  text(318.5,1.01,'9',cex=0.7, adj=c(0.5,0.5))
+          rect(337,0,374,1, col=MIS.col[4], border=NA)  ;  rect(337,1.02,374,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(355.5,1.01,'10',cex=0.7, adj=c(0.5,0.5))
+          rect(374,0,424,1, col=MIS.col[5], border=NA)  ;  rect(374,1.02,424,1, col="white", border='black', lwd=0.5)  ;  text(399,1.01,'11',cex=0.7, adj=c(0.5,0.5))
+          rect(424,0,478,1, col=MIS.col[4], border=NA)  ;  rect(424,1.02,478,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(451,1.01,'12',cex=0.7, adj=c(0.5,0.5))
+          rect(478,0,533,1, col=MIS.col[5], border=NA)  ;  rect(478,1.02,533,1, col="white", border='black', lwd=0.5)  ;  text(505.5,1.01,'13',cex=0.7, adj=c(0.5,0.5))
+          rect(533,0,563,1, col=MIS.col[4], border=NA)  ;  rect(533,1.02,563,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(548,1.01,'14',cex=0.7, adj=c(0.5,0.5))
+          rect(563,0,621,1, col=MIS.col[5], border=NA)  ;  rect(563,1.02,621,1, col="white", border='black', lwd=0.5)  ;  text(592,1.01,'15',cex=0.7, adj=c(0.5,0.5))
+          rect(621,0,676,1, col=MIS.col[4], border=NA)  ;  rect(621,1.02,676,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(648.5,1.01,'16',cex=0.7, adj=c(0.5,0.5))
+          rect(676,0,712,1, col=MIS.col[5], border=NA)  ;  rect(676,1.02,712,1, col="white", border='black', lwd=0.5)  ;  text(694,1.01,'17',cex=0.7, adj=c(0.5,0.5))
+          rect(712,0,761,1, col=MIS.col[4], border=NA)  ;  rect(712,1.02,761,1, col=MIS.col[4], border='black', lwd=0.5)  ;  text(736.5,1.01,'18',cex=0.7, adj=c(0.5,0.5))
+          rect(761,0,790,1, col=MIS.col[5], border=NA)  ;  rect(761,1.02,790,1, col="white", border='black', lwd=0.5)  ;  text(775.5,1.01,'19',cex=0.7, adj=c(0.5,0.5))
+          rect(790,0,809,1, col=MIS.col[4], border=NA)  ;  rect(790,1.02,809,1, col=MIS.col[4], border='black', lwd=0.5)  #;  text(790,1.015,'20',cex=0.7, adj=c(0.5,0.5))
 
           ## Bottom part
           rect(-9,0,809,1, col=NA, border="black", lwd=0.5)
-          text(400,1.035, "Marine Isotope Stages", cex=8/7, adj=c(0.5,0.5))
+          text(400,1.035, "Marine Isotope Stages", cex=6/7, adj=c(0.5,0.5))
           for(i in seq(0,800,25)){  segments(i,0,i,-0.004-ifelse(i%%50 == 0, 0.002,0), lwd=0.5)  }
-          for(i in seq(0,800,100)){  text(i,-0.01, i, cex=1, adj=c(0.5, 1))  }
-          text(400, -0.03, 'Age (calendar yr BP x1000)', adj=c(0.5,0.5), cex=8/7)
+          for(i in seq(0,800,100)){  text(i,-0.01, i, cex=6/7, adj=c(0.5, 1))  }
+          text(400, -0.035, 'Age (calendar yr BP x1000)', adj=c(0.5,0.5), cex=6/7)
       }
 
       ## Insolation
-      plot.window(xlim=c(-150,950),ylim=c(425,515)+90*c(-5.7,0)/0.7+90*c(-6.4*0.025, 6.4*0.025)/0.7,main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(425,515)+90*c(-5.7,0)/0.7+90*c(-6.4*0.025, 6.4*0.025)/0.7,main='',ylab='',xlab='')  ;  {
           COL='black'
           points(INSOL, col='grey70', type='l', lwd=1.2)
           for(i in seq(430,510,10)) segments(-20,i,-9,i, lwd=0.5, col=COL)
-          for(i in seq(430,510,20)) text(-25,i,i, adj=c(1,0.5), col=COL)
-          text(-175, 465, '(a) DJF Insolation\nat 20°S (W/m2)', adj=c(0.5,1), srt=90, col=COL, cex=8/7)
+          for(i in seq(430,510,20)) text(-25,i,i, adj=c(1,0.5), col=COL, cex=6/7)
+          text(-225, 465, '(a) DJF Insolation\nat 20°S (W/m2)', adj=c(0.5,1), srt=90, col=COL, cex=6/7)
       }
 
       ## Eccentricity
-      plot.window(xlim=c(-150,950),ylim=c(-0.04,0.06)+0.1*c(-6,0)/0.7+0.1*c(-6.4*0.025, 6.4*0.025)/0.7,main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(-0.04,0.06)+0.1*c(-6,0)/0.7+0.1*c(-6.4*0.025, 6.4*0.025)/0.7,main='',ylab='',xlab='')  ;  {
           COL='black'
           points(ECC, col='grey20', type='l', lwd=1.2)
           for(i in seq(0.004, 0.04,0.008)) segments(820,i,809,i, lwd=0.5, col=COL)
-          for(i in seq(0.004, 0.04,0.016)) text(825,i,i, adj=c(0,0.5), col=COL)
-          text(975, 0.022, '(b) Eccentricity', adj=c(0.5,0), srt=90, col=COL, cex=8/7)
+          for(i in seq(0.004, 0.04,0.016)) text(825,i,i, adj=c(0,0.5), col=COL, cex=6/7)
+          text(1025, 0.022, '(b) Eccentricity', adj=c(0.5,0), srt=90, col=COL, cex=6/7)
       }
 
       ## Pollen counts
-      plot.window(xlim=c(-150,950),ylim=c(0,450)+450*c(-5,0.7)/0.7+450*c(-6.4*0.025, 6.4*0.025)/0.7,main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(0,450)+450*c(-5,0.7)/0.7+450*c(-6.4*0.025, 6.4*0.025)/0.7,main='',ylab='',xlab='')  ;  {
           COL=makeTransparent('cornflowerblue', alpha=0.9)
           polygon(c(0,POLLENSUM[,1],POLLENSUM[181,1]), c(0,POLLENSUM[,3],0), col=COL, border=NA)
           COL=makeTransparent('forestgreen', alpha=0.9)
           polygon(c(0,POLLENSUM[,1],POLLENSUM[181,1]), c(0,POLLENSUM[,2],0), col=COL, border=NA)
           for(i in seq(0,400,50)) segments(820,i,809,i, lwd=0.5, col='black')
-          for(i in seq(0,400,100)) text(825,i,i, adj=c(0,0.5), col='black')
-          text(975, 225, '(c) Pollen counts\nTerrestrial & Aquatics', adj=c(0.5,0), srt=90, col='black', cex=8/7)
+          for(i in seq(0,400,100)) text(825,i,i, adj=c(0,0.5), col='black', cex=6/7)
+          text(1025, 225, '(c) Pollen counts\nTerrestrial & Aquatic', adj=c(0.5,0), srt=90, col='black', cex=6/7)
       }
 
       ## Pollen diversity
-      plot.window(xlim=c(-150,950),ylim=c(-5,55)+60*c(-4,1.4)+60*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(-5,55)+60*c(-4,1.4)+60*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
           COL='black'
           polygon(rep(TIME, each=2), c(0,rep(POLLENTAXA,each=2),0), col='forestgreen', border='forestgreen', lwd=1, lty=1, angle=-60, density=25)
           for(i in seq(0,50,5)) segments(-20,i,-9,i, lwd=0.5, col='black')
-          for(i in seq(0,50,10)) text(-25,i,i, adj=c(1,0.5), col='black')
-          text(-175, 25, '(d) Number of\npollen taxa per sample', adj=c(0.5,1), srt=90, col='black', cex=8/7)
+          for(i in seq(0,50,10)) text(-25,i,i, adj=c(1,0.5), col='black', cex=6/7)
+          text(-225, 25, '(d) Number of\npollen taxa per sample', adj=c(0.5,1), srt=90, col='black', cex=6/7)
           points(XX.interp, MARGALEF*5, col='grey20', type='l', lwd=1.2)
           for(i in seq(0, 9, 1.5)*5) segments(820,i,809,i, lwd=0.5, col=COL)
-          for(i in seq(0, 9, 3)*5) text(825,i,i/5, adj=c(0,0.5), col=COL)
-          text(975, 25, "(e) Margalef's diversity\nindex", adj=c(0.5,0), srt=90, col=COL, cex=8/7)
+          for(i in seq(0, 9, 3)*5) text(825,i,i/5, adj=c(0,0.5), col=COL, cex=6/7)
+          text(1025, 25, "(e) Margalef's diversity\nindex", adj=c(0.5,0), srt=90, col=COL, cex=6/7)
       }
 
       ## SST reconstruction
-      plot.window(xlim=c(-150,950),ylim=c(-3.3,3.2)+6.5*c(-3,2.4)+6.5*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(-3.3,3.2)+6.5*c(-3,2.4)+6.5*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
       COL='black'
           points(SSTs, col=makeTransparent('grey70', alpha=1), type='l', cex=0.3)
           points(XX.interp, SSTs.smooth, lwd=1.2, col='darkorchid3', type='l')
           for(i in seq(-3,3,1)) segments(-20,i,-9,i, lwd=0.5, col=COL)
-          for(i in seq(-3,3,2)) text(-25,i,i, adj=c(1,0.5), col=COL)
-          text(-175, -0.05, '(f) MD96-2048\nSSTs PC1', adj=c(0.5,1), srt=90, col=COL, cex=8/7)
+          for(i in seq(-3,3,2)) text(-25,i,i, adj=c(1,0.5), col=COL, cex=6/7)
+          text(-225, -0.05, '(f) MD96-2048\nSSTs PC1', adj=c(0.5,1), srt=90, col=COL, cex=6/7)
       }
 
       ## MAT reconstruction
-      plot.window(xlim=c(-150,950),ylim=c(16.9,21)+4.1*c(-2,3.4)+4.1*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(16.9,21)+4.1*c(-2,3.4)+4.1*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
           COL='black'
           points(MAT, col=makeTransparent('grey70', alpha=1), type='l', cex=0.3)
           points(XX.interp, MAT.smooth, lwd=1.2, col='darkorchid3', type='l')
           for(i in seq(16.5,21.5,0.5)) segments(820,i,809,i, lwd=0.5, col=COL)
-          for(i in seq(17,21.5,1)) text(825,i,i, adj=c(0,0.5), col=COL)
-          text(975, 19, '(g) MD96-2048 Pollen-based\nMAT Reconstruction (°C)', adj=c(0.5,0), srt=90, col=COL, cex=8/7)
+          for(i in seq(17,21.5,1)) text(825,i,i, adj=c(0,0.5), col=COL, cex=6/7)
+          text(1025, 19, '(g) MD96-2048 Pollen-based\nMAT Reconstruction (°C)', adj=c(0.5,0), srt=90, col=COL, cex=6/7)
       }
 
       ## Come C CO2
-      plot.window(xlim=c(-150, 950),ylim=c(175,300)+125*c(-1,4.4)+125*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200, 1000),ylim=c(175,300)+125*c(-1,4.4)+125*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
           COL='black'
           points(CO2 , col=makeTransparent('grey70', alpha=1), type='l', cex=0.3)
           points(XX.interp, CO2.smooth, lwd=1.2, col='darkorchid3', type='l')
           for(i in seq(180,300,20)) segments(-20,i,-9,i, lwd=0.5, col=COL)
-          for(i in seq(180,300,40)) text(-25,i,i, adj=c(1,0.5), col=COL)
-          text(-175, 237.5, '(h) Dome C, Antarctica\npCO2 (ppm)', adj=c(0.5,1), srt=90, col=COL, cex=8/7)
+          for(i in seq(180,300,40)) text(-25,i,i, adj=c(1,0.5), col=COL, cex=6/7)
+          text(-225, 237.5, '(h) Dome C, Antarctica\npCO2 (ppm)', adj=c(0.5,1), srt=90, col=COL, cex=6/7)
       }
 
       ## LR04
-      plot.window(xlim=c(-150,950),ylim=c(5,3.2)-1.8*c(0,5.4)-1.8*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
+      plot.window(xlim=c(-200,1000),ylim=c(5,3.2)-1.8*c(0,5.4)-1.8*c(-6.4*0.025, 6.4*0.025),main='',ylab='',xlab='')  ;  {
       COL='black'
           points(LR04, col=makeTransparent('grey70', alpha=1), type='l', cex=0.3)
           points(XX.interp, LR04.smooth, lwd=1.2, col='darkorchid3', type='l')
           for(i in seq(5,3.2,-0.25)) segments(820,i,809,i, lwd=0.5, col=COL)
-          for(i in seq(5,3.2,-0.5)) text(825,i,i, adj=c(0,0.5), col=COL)
-          text(975, 4.1, '(i) Global ice volume (LR04)\nd18Obenthic (permil VPDB)', adj=c(0.5,0), srt=90, col=COL, cex=8/7)
+          for(i in seq(5,3.2,-0.5)) text(825,i,i, adj=c(0,0.5), col=COL, cex=6/7)
+          text(1025, 4.1, '(i) Global ice volume (LR04)\nd18Obenthic (permil VPDB)', adj=c(0.5,0), srt=90, col=COL, cex=6/7)
       }
     dev.off()  ;  }
 
